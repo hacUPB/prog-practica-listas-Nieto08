c53faf95a1694cc86044e89face6cb3241ae20ca
JUAN NIETO <Juanchosnieto@hotmail.com>
2024-09-24 08:34:13 -0500

    agrego funcion de sumar matriz v0.1

--- src/ejercicios.py
+++ src/ejercicios.py
@@ -1,10 +1,14 @@
 # Ejercicio 1: Suma de elementos en una lista de listas
-def suma_matriz(matriz):
+def suma_matriz(matriz:list):
     """
     Recibe una lista de listas y devuelve la suma de todos sus elementos.
     Incluir el código aquí para sumar los elementos de la matriz.
     """
-    pass
+    suma= 0
+    for filas in matriz:
+        for dato in filas:
+            sumar+= dato
+    return suma
 
 # Ejercicio 2: Encontrar el valor máximo en una matriz
 def maximo_matriz(matriz):
